d66b186bbe9a5b73ad014bcabaa879f1f07eb42e
fummicc1 <fumiya.tennis1@gmail.com>
2019-08-02 13:26:34 +0900

    Fix the bug that the view goes out of the screen

--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -2,21 +2,10 @@ import UIKit
 import EasyAutoLayout
 
 class ViewController: EasyAutoLayoutViewController {
-
+    
     override func viewDidLoad() {
-        super.viewDidLoad()        
-    }
-    
-    override func viewWillLayoutSubviews() {
-        super.viewWillLayoutSubviews()
-        setupLayout()
-    }
-    
-    func setupLayout() {
-        let layoutViews = view.subviews.compactMap({$0 as? EasyAutoLayoutView})
-        for layoutView in layoutViews {
-            setAutoLayout(targetView: layoutView)
-        }
+        super.viewDidLoad()
+        view.subviews.forEach({setAutoLayout(targetView: $0)})        
     }
 }
 
--- EasyAutoLayout/EasyAutoLayout.swift
+++ EasyAutoLayout/EasyAutoLayout.swift
@@ -2,11 +2,7 @@ import Foundation
 
 open class EasyAutoLayoutViewController: UIViewController {
     
-    private func fixLayout() {
-        
-    }
-    
-    open func setAutoLayout(targetView target: EasyAutoLayoutView, merge: Bool = false, _ parent: UIView? = nil) {
+    open func setAutoLayout(targetView target: UIView, merge: Bool = false, _ parent: UIView? = nil) {
         var parent = parent
         if parent == nil {
             parent = target.superview
@@ -18,18 +14,7 @@ open class EasyAutoLayoutViewController: UIViewController {
     }
 }
 
-open class EasyAutoLayoutView: UIView {
-    
-    open var priority: EasyPriority
-    
-    public init(frame: CGRect, priority: EasyPriority = .center) {
-        self.priority = priority
-        super.init(frame: frame)
-    }
-    
-    required public init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
+extension UIView {
     
     func setAutoLayout(merge: Bool, parentView: UIView) {
         if !merge {
@@ -42,21 +27,29 @@ open class EasyAutoLayoutView: UIView {
         var leftDistance = frame.origin.x
         var rightDistance = parentView.bounds.width - frame.origin.x - frame.width
         
+        if topDistance < 0 {
+            bottomDistance += -topDistance + 32
+            topDistance = 32
+        }
+        if bottomDistance < 0 {
+            topDistance -= -bottomDistance + 32
+            bottomDistance = 32
+        }
+        if leftDistance < 0 {
+            rightDistance = -leftDistance + 32
+            leftDistance = 32
+        }
+        if rightDistance < 0 {
+            leftDistance = -rightDistance + 32
+            rightDistance = 32
+        }
         
         let topConstraint = NSLayoutConstraint(item: self, attribute: .top, relatedBy: .equal, toItem: parentView, attribute: .top, multiplier: 1.0, constant: topDistance)
-        let bottomConstraint = NSLayoutConstraint(item: self, attribute: .bottom, relatedBy: .equal, toItem: parentView, attribute: .bottom, multiplier: 1.0, constant: bottomDistance)
+        let bottomConstraint = NSLayoutConstraint(item: self, attribute: .bottom, relatedBy: .equal, toItem: parentView, attribute: .bottom, multiplier: 1.0, constant: -bottomDistance)
         let leftConstraint = NSLayoutConstraint(item: self, attribute: .left, relatedBy: .equal, toItem: parentView, attribute: .left, multiplier: 1.0, constant: leftDistance)
-        let rightConstraint = NSLayoutConstraint(item: self, attribute: .right, relatedBy: .equal, toItem: parentView, attribute: .right, multiplier: 1.0, constant: rightDistance)
-        addConstraints([topConstraint, bottomConstraint, leftConstraint, rightConstraint])
-        setNeedsLayout()
-        layoutIfNeeded()
+        let rightConstraint = NSLayoutConstraint(item: self, attribute: .right, relatedBy: .equal, toItem: parentView, attribute: .right, multiplier: 1.0, constant: -rightDistance)
+        translatesAutoresizingMaskIntoConstraints = false
+        parentView.addConstraints([topConstraint, bottomConstraint, leftConstraint, rightConstraint])
+        parentView.setNeedsLayout()
     }
-}
-
-public enum EasyPriority {
-    case center
-    case left
-    case right
-    case top
-    case bottom
 }
